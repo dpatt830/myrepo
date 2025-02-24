07b8ceec4167e25bc2df2a468f577fc737f85f29
dpatt830 <120147120+dpatt830@users.noreply.github.com>
2025-02-24 09:47:19 -0600

    Create sleuth.R

---
+++ sleuth.R
@@ -0,0 +1,27 @@
+#load package
+library(sleuth)
+
+#read in the table you made describing samples and kallisto output, 
+#assign to variable name stab 
+stab = read.table("metadata.txt",header=TRUE)
+
+#initialize sleuth object using sleuth_prep function from sleuth library
+so = sleuth_prep(stab)
+
+#fit a model comparing the two conditions 
+so = sleuth_fit(so, ~condition, 'full')
+#fit the reduced model to compare in the likelihood ratio test so = sleuth_fit(so, ~1, 'reduced')
+so = sleuth_fit(so, ~1, 'reduced')
+#perform the likelihood ratio test for differential expression between conditions 
+so = sleuth_lrt(so, 'reduced', 'full')
+
+#load the dplyr package for data.frame filtering
+library(dplyr)
+#extract the test results from the sleuth object 
+sleuth_table = sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE) 
+
+#filter most significant results (FDR/qval < 0.05) and sort by pval
+sleuth_significant = dplyr::filter(sleuth_table, qval <= 0.05) |> dplyr::arrange(pval) 
+
+#write FDR < 0.05 transcripts to file
+write.table(sleuth_significant, file="fdr05_results.txt",quote = FALSE,row.names = FALSE)
